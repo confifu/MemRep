4ce9f1806a3e1abc0c8caf2d381198dcd0c4b3a9
Saurabh Kumar <saurabhk660@gmail.com>
2021-06-06 11:17:30 +0530

    Change model architecture and add writeheads
    Following changes have been made:
    * Modify write head to take in (batch, frame, embed)
    * Each frame votes on every row of memory for erase
      and add operations. The avergae vote for each row
      is taken.
    * Remove read head. RA is enough imo.
    * Add the redundant attention class. Idea is to use
      TSM and MHA to select repeating frames. Since we
      also need frame embeddings also using actual frame
      embeddings and their difference. Also this class
      can be stacked so we can get more abstract features.

--- NTM.py
+++ NTM.py
@@ -8,34 +8,31 @@ class Memory(nn.Module):
     def __init__(self, numRows, numCols):
         super(Memory, self).__init__()
 
-        self.numCols = numCols
-        self.numRows = numRows
+        self.num_cols = numCols
+        self.num_rows = numRows
         self.mem_bias = torch.Tensor().new_full((numRows, numCols), 1e-6)
         
     def init_state(self, batch_size, device):
         self.data =  self.mem_bias.clone().repeat(batch_size, 1, 1).to(device)
 
-class HeadBase(nn.Module):
+
+
+class WriteHead(nn.Module):
     
     def __init__(self, memory, hidden_size, max_shift):
-        super(HeadBase, self).__init__()
+        super(WriteHead, self).__init__()
         self.memory = memory
         self.hidden_size = hidden_size
         self.max_shift = max_shift
+        self.mha = nn.MultiheadAttention(embed_dim=self.memory.num_cols, num_heads=4)
 
         self.fc = nn.Linear(hidden_size,
                             sum(s for s, _ in self.hidden_state_unpacking_scheme()))
-        self.init_params()
+        #self.init_params()
 
-    def forward(self, h):
-        raise NotImplementedError
-
-    def hidden_state_unpacking_scheme():
-        raise NotImplementedError
-           
     def unpack_hidden_state(self, h):
         chunk_idxs, activations = zip(*self.hidden_state_unpacking_scheme())
-        chunks = torch.split(h, chunk_idxs, dim=1)
+        chunks = torch.split(h, chunk_idxs, dim=-1)
         return tuple(activation(chunk) for chunk, activation in zip(chunks, activations))
 
     def focus_head(self, k, beta, prev_w, g, s, gamma):
@@ -46,9 +43,20 @@ class HeadBase(nn.Module):
         return w
 
     def _content_weight(self, k, beta):
+        '''
         k = k.unsqueeze(1).expand_as(self.memory.data)
         similarity_scores = F.cosine_similarity(k, self.memory.data, dim=2)
-        w = F.softmax(beta * similarity_scores, dim = 1)
+        '''
+        self.memory.data = self.memory.data.transpose(0, 1)
+        k = k.transpose(0, 1)
+        _, similarity_scores = self.mha(self.memory.data, k, k)
+        self.memory.data = self.memory.data.transpose(0, 1)
+        k = k.transpose(0, 1)
+        similarity_scores = similarity_scores.transpose(-1, -2)
+
+        #print(similarity_scores.shape, beta.shape)
+        w = F.softmax(beta * similarity_scores, dim = -1)
+        #print(similarity_scores.shape, w.shape)
         return w
     
 
@@ -56,59 +64,17 @@ class HeadBase(nn.Module):
         return g*w + (1-g)*prev_w
 
     def _mod_shift(self, w , s):
-        unrolled = torch.cat([w[:, -self.max_shift:], w, w[:, :self.max_shift]], 1)
+        unrolled = torch.cat([w[:,:, -self.max_shift:], w, w[:,:, :self.max_shift]], -1)
         return F.conv1d(unrolled.unsqueeze(1), 
                         s.unsqueeze(1))[range(self.batch_size), range(self.batch_size)]
     
     def _sharpen(self, w, gamma):
         w = w.pow(gamma)
-        return torch.div(w, w.sum(1).view(-1, 1) + 1e-16)
+        return torch.div(w, w.sum(-1).unsqueeze(-1) + 1e-16)
 
 
     def init_state(self, batch_size):
         self.batch_size = batch_size
-
-    def init_params(self):
-        pass
-
-
-class ReadHead(HeadBase):
-
-    def __init__(self, memory, hidden_sz, max_shift):
-        super(ReadHead, self).__init__(memory, hidden_sz, max_shift)
-
-        
-    def hidden_state_unpacking_scheme(self):
-      return [
-            # size, activation-function
-            (self.memory.num_cols, torch.tanh),                    # k
-            (1,                    F.softplus),                    # β
-            (1,                    torch.sigmoid),                 # g
-            (2*self.max_shift+1,   lambda x: F.softmax(x, dim=1)), # s
-            (1,                    lambda x: 1 + F.softplus(x))    # γ
-        ]
-        
-    def read(self, w):
-        return torch.matmul(w.unsqueeze(1), self.memory.data).squeeze(1)
-
-    def forward(self, h, prev_w):
-        k, beta, g, s, gamma = self.unpack_hidden_state(self.fc(h))
-        w = self.focus_head(k, beta, prev_w, g, s, gamma)
-        read = self.read(w)
-        return read, w
-    
-    def init_state(self, batch_size, device):
-        self.batch_size = batch_size
-        reads = torch.zeros(batch_size, self.memory.num_cols).to(device)
-        read_focus = torch.zeros(batch_size, self.memory.num_cols).to(device)
-        read_focus[:, 0] = 1.
-        return reads, read_focus
-
-
-class WriteHead(HeadBase):
-
-    def __init__(self, memory, hidden_sz, max_shift):
-        super(WriteHead, self).__init__(memory, hidden_sz, max_shift)
 
     def hidden_state_unpacking_scheme(self):
         return [
@@ -122,21 +88,28 @@ class WriteHead(HeadBase):
             (self.memory.num_cols, torch.tanh)                     # a
         ] 
         
-    def erase(self, w, e):
-        return self.memory.data * (1 - w.unsqueeze(2) * e.unsqueeze(1))
     
     def write(self, w, e, a):
-        memory_erased = self.erase(w, e)
-        self.memory.data = memory_erased + (w.unsqueeze(2) * a.unsqueeze(1))
+
+        if w.dim == 2:
+            w = w.unsqueeze(1)
+            e = e.unsqueeze(1)
+            a = a.unsqueeze(1)
+        w = w.transpose(-1, -2)
+
+        memory_erased = self.memory.data * torch.einsum('bmh,bhe->bme', (w, e))
+        self.memory.data = memory_erased + torch.einsum('bmh,bhe->bme', (w, a))
+
 
     def forward(self, h, prev_w):
         k, beta, g, s, gamma, e, a = self.unpack_hidden_state(self.fc(h))
         w = self.focus_head(k, beta, prev_w, g, s, gamma)
+
         self.write(w, e, a)
         return w
 
-    def init_state(self, batch_size, device):
+    def init_state(self, batch_size,fpv, device):
         self.batch_size = batch_size
-        write_focus = torch.zeros(batch_size, self.memory.num_rows).to(device)
-        write_focus[:, 0] = 1.
+        write_focus = torch.zeros(batch_size, fpv, self.memory.num_rows).to(device)
+        write_focus[:,:, 0] = 1.
         return write_focus
